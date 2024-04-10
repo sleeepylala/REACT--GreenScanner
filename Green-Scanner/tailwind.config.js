fc9451f6fa13f5aacb1a988088a7706958391002
ilaria <ilariaferrauti@gmail.com>
2024-04-10 15:52:55 +0200

    varie config

--- Green-Scanner/tailwind.config.js
+++ Green-Scanner/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Definisci i colori per la modalità light
+        primary: "#00BFA6",
+        secondary: "#FFFFFF",
+        textColor: "#263E48",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        "red-hat-display": ["Red Hat Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
