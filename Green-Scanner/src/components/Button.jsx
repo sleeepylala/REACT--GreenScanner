c73be171a0915558a6844174521ac39caab3d6e7
ilaria <ilariaferrauti@gmail.com>
2024-05-13 19:40:14 +0200

    animazione bottone

--- Green-Scanner/src/components/Button.jsx
+++ Green-Scanner/src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Button = ({ href, onSubmit }) => {
   const handleClick = (e) => {
@@ -8,13 +9,15 @@ const Button = ({ href, onSubmit }) => {
   };
 
   return (
-    <a
+    <motion.a
       href={href}
       onClick={handleClick}
       className="bg-primary btn-hero border-white border-2 rounded-full py-5 lg:px-20 px-10  text-white font-red-hat-display"
+      whileTap={{ scale: 0.8 }} // Imposta l'animazione quando il tasto è premuto
+      whileHover={{ scale: 1.2 }} // Imposta l'animazione quando il tasto è in hover
     >
       calculate now
-    </a>
+    </motion.a>
   );
 };
 
